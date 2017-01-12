9deaed2ad454e45c7d99935374d804ba58dfb585
DinhTran <mrtrandinhvn@gmail.com>
2017-01-12 09:43:30 +0700

    Initializing project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+﻿var path = require("path");
+var webpack = require("webpack");
+var autoprefixer = require("autoprefixer");
+module.exports = {
+    context: path.join(__dirname),
+    entry: {
+        index: "./js/index.jsx"
+    },
+    output: {
+        path: path.join(__dirname + "/js/built/"),
+        filename: "[name].bundle.js".toLowerCase()
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$|\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel",
+                query: {
+                    presets: ["es2015", "react"],
+                    plugins: ["transform-object-rest-spread"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
+            }
+        ]
+    },
+    postcss: [autoprefixer({ browsers: ["last 2 versions"] })],
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        extensions: ["", ".js", ".jsx"],
+        root: [
+
+        ]
+    },
+    externals: {
+
+    }
+};
