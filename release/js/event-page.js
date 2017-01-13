6551589ef28da7b9bd6dbc1203483e75921724d5
mrtrandinhvn <mrtrandinhvn@gmail.com>
2017-01-13 18:49:48 +0700

    exclude release folder

--- release/js/event-page.js
+++
@@ -1,4 +0,0 @@
-// React when a browser action's icon is clicked.
-chrome.browserAction.onClicked.addListener((tab) => {
-    window.open(chrome.extension.getURL("html/index.html"), "_target");
-});
